7e32a8444cd659c6c797eb4c6a00b4f9dac0a6ce
RaviGoel260794 <mfsi.ravigoel@gmail.com>
2025-03-02 21:12:35 -0800

    Removed space from UITEst

--- com.demo.git/src/test/java/mavenforjenkins/UITest.java
+++ com.demo.git/src/test/java/mavenforjenkins/UITest.java
@@ -36,8 +36,6 @@ public class UITest {
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"),"Title does not match");
 		driver.quit();
-		
-		
 	}
 
 }
